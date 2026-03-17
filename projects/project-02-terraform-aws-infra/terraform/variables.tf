ceda9b99d0482caacb0bd5d62f7ae7b9c9e4c096
Olanrewaju Eniola <olaeniola16@gmail.com>
2026-03-17 12:02:14 +0000

    Initialize Project 02 Terraform AWS infrastructure scaffold

---
+++ projects/project-02-terraform-aws-infra/terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS region for Terraform resources"
+  type        = string
+  default     = "eu-west-2"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "instance_name" {
+  description = "Name tag for the EC2 instance"
+  type        = string
+  default     = "terraform-project-02-instance"
+}
